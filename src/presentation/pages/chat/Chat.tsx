c73d82196d250c36088c514fd01219d309a47eda
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-21 21:19:33 -0600

    Iniciando el estilo del chat

--- src/presentation/pages/chat/Chat.tsx
+++ src/presentation/pages/chat/Chat.tsx
@@ -1,9 +1,15 @@
-import { NavBar } from "../../components"
+
 
 export const Chat = () => {
   return (
     <>
-      <NavBar/>
+      <main className="grid grid-cols-3">
+        <div className="col-span-2 bg-indigo-800 h-screen overflow-y-scroll overflow-x-auto">
+        </div>
+
+        <div className="col-span-1">
+        </div>
+      </main>
     </>
   )
 }
