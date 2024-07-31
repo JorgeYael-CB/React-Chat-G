cfdd7fe705bea659b36a6bbc110cb76a1f9a9a62
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-31 09:24:07 -0600

    Mejorando diseno

--- src/presentation/pages/chat/components/UserData.tsx
+++ src/presentation/pages/chat/components/UserData.tsx
@@ -4,16 +4,22 @@ import { FaEdit } from 'react-icons/fa';
 export const UserData = ({ user }: { user: UserInterface }) => {
   return (
     <main className="shadow-md bg-gray-100 py-10">
-      <div className="flex flex-col items-center bg-white mx-auto max-w-xl rounded-lg p-6 border-4 border-blue-700 my-8 shadow-lg">
-        <img
-          src={user.img}
-          className="w-32 h-32 rounded-full border-4 border-blue-500"
-          alt="Profile image"
-        />
+      <div className="flex flex-col items-center bg-white mx-auto max-w-xl rounded-lg p-6 border-4 border-blue-700 my-8 shadow-lg relative">
+        <div className="relative">
+          <img
+            src={user.img}
+            className="w-32 h-32 rounded-full border-4 border-blue-500"
+            alt="Profile image"
+          />
+          <FaEdit className="text-blue-500 cursor-pointer hover:text-blue-700 absolute top-0 right-0 transform translate-x-1/4 -translate-y-1/4" />
+        </div>
         <h1 className="font-semibold text-2xl mt-4">{user.name}</h1>
         <div className="mt-4 flex items-center gap-2">
           <img src={`/countrys/${user.country.toLowerCase()}.png`} className="w-10 h-10 rounded" alt="Country Flag" />
-          <p className="text-lg text-gray-700">{user.country}</p>
+          <div className='flex gap-4 items-center'>
+            <p className="text-lg text-gray-700">{user.country}</p>
+            <FaEdit className="text-blue-500 cursor-pointer hover:text-blue-700" />
+          </div>
         </div>
       </div>
 
@@ -42,15 +48,6 @@ export const UserData = ({ user }: { user: UserInterface }) => {
               <p className="text-lg text-gray-700">********</p>
               <FaEdit className="text-blue-500 cursor-pointer hover:text-blue-700" />
             </div>
-          </div>
-          <hr className="border-t-2 border-gray-300" />
-          <div className="flex justify-between items-center">
-            <h3 className="text-lg text-gray-700">Profile image:</h3>
-            <FaEdit className="text-blue-500 cursor-pointer hover:text-blue-700" />
-          </div>
-          <div className="flex justify-between items-center">
-            <h3 className="text-lg text-gray-700">Country:</h3>
-            <FaEdit className="text-blue-500 cursor-pointer hover:text-blue-700" />
           </div>
         </div>
       </div>
