16bd0c59435cbda692eade58fb5fc3d2a288fcb9
JorgeYael-CB <jorge.yael333@gmail.com>
2024-08-09 14:37:51 -0600

    Agregando proteccion de rutas y nueva route para el chat con el servidor

--- src/presentation/router/Routes.tsx
+++ src/presentation/router/Routes.tsx
@@ -2,8 +2,36 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import { ForgotPassword, Login, Register, ResetPassword } from '../pages/auth';
 import { Chat } from "../pages/chat";
 import { PrivateAcces } from "../pages/auth/PrivateAcces";
+import { ReactElement } from "react";
 
 
+interface Route {
+  path: string;
+  element: ReactElement;
+}
+
+const authRoutes:Route[] = [
+  {
+    path: 'login/',
+    element: <Login/>,
+  },
+  {
+    path: 'register/',
+    element: <Register/>,
+  },
+  {
+    path: 'reset-password/',
+    element: <ResetPassword/>,
+  },
+  {
+    path: 'forgot-password/',
+    element: <ForgotPassword/>,
+  },
+  {
+    path: '/auth/*',
+    element: <Navigate to='/auth/login'/>
+  }
+];
 
 
 export const Routes = createBrowserRouter([
@@ -12,28 +40,16 @@ export const Routes = createBrowserRouter([
     element: <Chat/>,
   },
   {
-    path: 'auth/',
+    path: '/',
     element: <PrivateAcces/>,
     children: [
       {
-        path: 'login/',
-        element: <Login/>,
+        path: 'auth/',
+        children: authRoutes,
       },
       {
-        path: 'register/',
-        element: <Register/>,
-      },
-      {
-        path: 'reset-password/',
-        element: <ResetPassword/>,
-      },
-      {
-        path: 'forgot-password/',
-        element: <ForgotPassword/>,
-      },
-      {
-        path: '/auth/*',
-        element: <Navigate to='/auth/login'/>
+        path: 'chat/:serverId',
+        element: <h1>Hello World</h1>
       }
     ]
   },
